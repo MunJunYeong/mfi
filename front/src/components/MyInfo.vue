510448d361852ad8dd9821706311378d0ce03d11
moonjunyeong <>
2022-02-14 16:38:46 +0900

    mobile version update

--- front/src/components/MyInfo.vue
+++ front/src/components/MyInfo.vue
@@ -1,17 +1,16 @@
 <template>
     <v-container>
-        <br>
-        <v-row>
+        <!-- pc version -->
+        <br class="pc">
+        <v-row class="pc">
             <v-col  cols='1' />
             <v-col cols='9' id="title"> 
                 개인정보
                 <hr>
             </v-col>
-            
         </v-row>
-        
-        <br>
-        <v-container >
+        <br class="pc">
+        <v-container class="pc">
             <v-row>
                 <v-col  cols='1' />
                 <v-col cols='3'>
@@ -52,9 +51,54 @@
                 </v-col>
             </v-row>
         </v-container>
-
-        
+        <!-- mobile version -->
+        <!-- <br class="mobile"> -->
+        <v-row class="mobile">
+            <v-col cols='1'></v-col>
+            <v-col cols='11' id="mobileTitle"> 
+                개인정보
+                <hr>
+            </v-col>
+        </v-row>
+        <v-container class="mobile">
+            <v-row>
+                <v-col cols='5' class="mobileSubject">
+                    닉네임
+                </v-col>
+                <v-col cols='7' class="mobileSubject"  >
+                    {{nickName}}
+                </v-col>
+            </v-row>
+            <br><br>
+            <v-row>
+                <v-col cols='5' class="mobileSubject">
+                    이메일
+                </v-col>
+                <v-col cols='7' class="mobileSubject">
+                    {{email}}
+                </v-col>
+            </v-row>
+            <br><br>
+            <v-row>
+                <v-col cols='5' class="mobileSubject">
+                    등급
+                </v-col>
+                <v-col cols='7' class="mobileSubject">
+                    {{role}}
+                </v-col>
+            </v-row>
+            <br><br>
+            <v-row>
+                <v-col cols='5' class="mobileSubject">
+                    가입일
+                </v-col>
+                <v-col cols='7' class="mobileSubject">
+                    {{createdAt}}
+                </v-col>
+            </v-row>
+        </v-container>
     </v-container>
+    
 </template>
 <script>
 import moment from 'moment';
@@ -92,7 +136,30 @@ export default {
 }
 </script>
 <style scoped>
+@media all and (max-width:767px) {
+    .pc{
+        display: none;
+    }
+}
+@media all and (max-width:1023px) and (min-width:767px) {
+    .mobile{
+        display: none;
+    }
+    /* 태블릿은 잘보임 */
+}
+@media all and (min-width:1024px) {
+    .mobile{
+        display: none;
+    }
+}
+
     #title{
         font-size: 2em; height: 80px;
+    }
+    #mobileTitle{
+        font-size: 1.3em; height: 80px;
+    }
+    .mobileSubject{
+        font-size: 0.8em;
     }
 </style>
