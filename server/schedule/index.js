f5edcf7a8864b83a84a986b8912b17567a563e11
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-02-23 16:02:13 +0900

    Revert "schedule"
    This reverts commit a6927f48d110376b7dcb204ea0d00a1ca7912376.

--- server/schedule/index.js
+++ server/schedule/index.js
@@ -1,7 +1,7 @@
 const {visitor : visitorService} = require('../service');
 const schedule = require('node-schedule');
 
-const addTotal = schedule.scheduleJob('0 * * * * *', async ()=>{
+const addTotal = schedule.scheduleJob('0 0 0 * * *', async ()=>{
     const totalCnt = await visitorService.getTotalVisitor();
     const updateTotal = await visitorService.updateTotalVisitor(parseInt(totalCnt));
     if(parseInt(updateTotal)){
