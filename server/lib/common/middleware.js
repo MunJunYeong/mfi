515a1312d884e449579b0b9bc6020585fbe10b94
moonjunyeong <>
2022-04-08 22:28:36 +0900

    feedback (error no -> error message) (middleware merge) (need visitor api)

--- server/lib/common/middleware.js
+++ server/lib/common/middleware.js
@@ -4,9 +4,6 @@ const userService = require('../../service/user');
 
 //검증할 때 토큰이 유효한지 + 저장된 토큰과 일치한지도 확인
 
-
-
-
 const validateToken = async (req, res, next) => {
     let token = req.headers.authorization;
     let userData;
@@ -43,37 +40,35 @@ const validateToken = async (req, res, next) => {
 
 // front index에서 토큰이 유효한지에 대한 검사
 // logout 일때는 유효한 토큰이고, 유효한 토큰이 아닐 시에는
-const verifyToken = async (req, res) => {
-    let token = req.headers.authorization;
-    let userData;
+// const verifyToken = async (req, res) => {
+//     let token = req.headers.authorization;
+//     let userData;
 
-    if(!token){
-        res.send({message : 'need token'});
-        return;
-    }
-    token = token.replace("Bearer ",  "");
-    try{
-        userData = jwt.verify(token, 'shhhhh');
-    }catch(err){
-        console.log('verify 비정상적인 토큰')
-        userService.forceLogout(token);
-        res.send({message : 'unvalid token'});
-        return;
-    }
+//     if(!token){
+//         res.send({message : 'need token'});
+//         return;
+//     }
+//     token = token.replace("Bearer ",  "");
+//     try{
+//         userData = jwt.verify(token, 'shhhhh');
+//     }catch(err){
+//         console.log('verify 비정상적인 토큰')
+//         userService.forceLogout(token);
+//         res.send({message : 'unvalid token'});
+//         return;
+//     }
     
-    let dataToken = await userService.getUserToken(userData.userIdx);
+//     let dataToken = await userService.getUserToken(userData.userIdx);
     
-    if(token !== dataToken){
-        await res.send({message : 'force logout'});
-        return;
-    }else {
-        res.send({data : 1});
-    }
-
-
-}
+//     if(token !== dataToken){
+//         await res.send({message : 'force logout'});
+//         return;
+//     }else {
+//         res.send({data : 1});
+//     }
+// }
 
 module.exports = {
     validateToken,
-    verifyToken
+    // verifyToken
 };
