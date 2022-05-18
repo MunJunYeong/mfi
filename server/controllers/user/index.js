e2c9d8d90cdab3938b0022578f7657d66d1f8d67
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-05-19 00:25:30 +0900

    controllers if else문 정리

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -7,22 +7,15 @@ const {Op} = require('../../lib/db');
 const updateUserRole = async (req, res) => {
     const data = req.body;
     
-    if(!data.role || !data.userIdx){
-        throw new Error('WRONG_ACCESS');
-    }
+    if(!data.role || !data.userIdx)throw new Error('WRONG_ACCESS');
 
     try{
         const result = await userService.updateRole(data.role, data.userIdx);
         res.send(result)
     }catch(err){
-        if(err.message === 'DB_NOT_FOUND_USER'){
-            throw new Error(err.message);
-        }else if(err.message === 'DB_UPDATE_ROLE'){
-            throw new Error(err.message);
-        }else {
-            winston.error(`Unable to updateUserRole :`, err);
-            throw new Error('UNABLE_USERROLE');
-        }
+        if(err.message === 'DB_NOT_FOUND_USER')throw new Error(err.message);
+        if(err.message === 'DB_UPDATE_ROLE')throw new Error(err.message);
+        winston.error(`Unable to updateUserRole :`, err);throw new Error('UNABLE_USERROLE');
     }
 }
 
@@ -34,17 +27,12 @@ const getuserData = async(req, res) => {
         data = await userService.getUserData(userIdx);
         res.send({data : data});
     }catch(err){
-        if(err.message === 'DB_GET_USER_DATA'){
-            throw new Error(err.message);
-        }else {
-            winston.error(`Unable to getUserData :`, err);
-            throw new Error('UNABLE_GET_USER_DATA');
-        }
+        if(err.message === 'DB_GET_USER_DATA')throw new Error(err.message);
+        winston.error(`Unable to getUserData :`, err);throw new Error('UNABLE_GET_USER_DATA');
     }
 }
 const getUser = async (req, res)=> {
     const {page, nickName} = req.query;
-            
     const {limit, offset} = pagination.getPagination(page);
         
     let where = {};
@@ -53,8 +41,7 @@ const getUser = async (req, res)=> {
         [Op.ne] : 'admin'
     }
     if(nickName !== undefined){
-        where.nickName = {
-            [Op.like] : `%${nickName}%`
+        where.nickName = {[Op.like] : `%${nickName}%`
         }
     }
     try{
@@ -62,12 +49,8 @@ const getUser = async (req, res)=> {
         const result = pagination.getPagingUserData(data, page, limit);
         res.send(result);
     }catch(err){
-        if(err.message === 'DB_GET_USER'){
-            throw new Error(err.message);
-        }else {
-            winston.error(`Unable to getUser(role:admin) :`, err);
-            throw new Error('UNABLE_GET_USER');
-        }
+        if(err.message === 'DB_GET_USER')throw new Error(err.message);
+        winston.error(`Unable to getUser(role:admin) :`, err);throw new Error('UNABLE_GET_USER');
     }
 
 }
