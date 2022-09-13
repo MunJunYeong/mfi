ac013d37e08127193391881e642830385a9d225e
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-09-13 23:34:06 +0900

    typeORM 1:M 매핑관계  연습

--- server2/src/user/entities/user.entity.ts
+++ server2/src/user/entities/user.entity.ts
@@ -1,5 +1,6 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import * as argon2 from "argon2";
+import { Product } from "src/product/entities/product.entity";
 
 @Entity('user')
 export class User {
@@ -14,6 +15,10 @@ export class User {
 
     @Column({length:200})
     name: string;
+
+    @OneToMany(()=> Product, (product)=> product.user, {
+        onDelete : 'CASCADE'
+    })
 
     // DB에 넣기 전에 비밀번호 복호화 할 수 없게 hash함수를 이용해 저장
     @BeforeInsert()
