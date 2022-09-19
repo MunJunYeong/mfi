9df4c6bc3dcb8cb3a333aeec177ccb93731d34c0
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-09-19 23:48:31 +0900

    winston - logging 1/2

--- server2/src/lib/common/middleware/logger.middleware.ts
+++ server2/src/lib/common/middleware/logger.middleware.ts
@@ -1,10 +1,23 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
-import { NextFunction } from "express";
+import { Inject, Injectable, LoggerService, NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction } from 'express';
+import { Logger } from "winston";
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+    constructor(@Inject(Logger) private readonly logger: LoggerService){}
+
     use(req: Request, res: Response, next: NextFunction){
         console.log('middleware request!!!')
+        const {ip, method, originalUrl} = req;
+        const userAgent = req.get('user-agent');
+
+        // res.on('finish', ()=> {
+        //     const {statusCode} = res;
+        //     this.logger.log(
+        //         `${method} ${originalUrl} ${statusCode} ${ip} ${userAgent}`,
+        //     );
+        // });
+
         next();
     }
 }
