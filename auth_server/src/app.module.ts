9d57b8ab6de61439658c7521249ce82f6c9e2ef4
moonjunyeong <>
2022-09-26 18:03:24 +0900

    graphQL - Schema first, code first 중에서 스키마 퍼스트로 선택(확정은 아님) test server에서 만들어둔 config service + typeorm으로 적용 완료

--- auth_server/src/app.module.ts
+++ auth_server/src/app.module.ts
@@ -1,9 +1,36 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigModule } from './configs/db/config.module';
+import { PostgresConfigService } from './configs/db/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // graphQL import
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+    }),
+    // Config import
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath : `./src/configs/env/.${process.env.NODE_ENV}.env`
+    }),
+    //TypeORM import
+    TypeOrmModule.forRootAsync({
+      imports : [PostgresConfigModule],
+      useClass : PostgresConfigService,
+      inject : [PostgresConfigService]
+    }),
+
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ auth_server/src/user/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Injectable()
+export class UserService {
+  findId(email: String) {
+    return email;
+  }
+}
