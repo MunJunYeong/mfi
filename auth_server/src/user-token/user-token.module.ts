439a86ffdcdb2f4490ccf7ecabd997bcde14202b
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-10-14 14:23:22 +0900

    auth_server 삭제 후 user_server 사용!!!

--- auth_server/src/user-token/user-token.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserTokenService } from './user-token.service';
-import { UserTokenResolver } from './user-token.resolver';
-
-@Module({
-  providers: [UserTokenResolver, UserTokenService]
-})
-export class UserTokenModule {}
--- auth_server/src/user-token/user-token.resolver.ts
+++
@@ -1,8 +0,0 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { UserTokenService } from './user-token.service';
-import { UserToken } from './entities/user-token.entity';
-
-@Resolver(() => UserToken)
-export class UserTokenResolver {
-  constructor(private readonly userTokenService: UserTokenService) {}
-}
