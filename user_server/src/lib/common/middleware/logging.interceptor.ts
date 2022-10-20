efc00801d7cc84b06878eff5041ab22b37df8a01
moonjunyeong <>
2022-10-20 17:21:52 +0900

    인터셉터에서 req 가져오는 것 찾음

--- user_server/src/lib/common/middleware/logging.interceptor.ts
+++ user_server/src/lib/common/middleware/logging.interceptor.ts
@@ -1,15 +1,19 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { JwtService } from '../jwt';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  constructor(
+    private readonly jwtService: JwtService,
+){}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest();
-    console.log(request)
-    //   console.log(value.input.userIdx);
-    //   console.log(context.getArgs());
-    const now = Date.now();
+    const ctx = GqlExecutionContext.create(context);
+    const req = ctx.getContext().req;
+
+
     return next
       .handle()
       .pipe(
